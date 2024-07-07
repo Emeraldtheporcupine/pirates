37ddc7ddd24a4c3f9fbf8b2003e02986809d4bd9
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2024-07-07 12:47:35 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,96 @@
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    Facing_L = true
+    Facing_R = false
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Pirate Stand L`,
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    Facing_R = true
+    Facing_L = false
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Pirate Stand`,
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Facing_R = true
+    Facing_L = false
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Pirate Walk`,
+    200,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Facing_L = true
+    Facing_R = false
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`Pirate Walk0`,
+    200,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Facing_R == true) {
+        Facing_R = true
+        Facing_L = false
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`Pirate Walk`,
+        200,
+        true
+        )
+    } else if (Facing_L == true) {
+        Facing_L = true
+        Facing_R = false
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`Pirate Walk0`,
+        200,
+        false
+        )
+    }
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    if (Facing_R == true) {
+        Facing_R = true
+        Facing_L = false
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`Pirate Stand`,
+        200,
+        true
+        )
+    } else if (Facing_L == true) {
+        Facing_L = true
+        Facing_R = false
+        animation.runImageAnimation(
+        mySprite,
+        assets.animation`Pirate Stand L`,
+        200,
+        false
+        )
+    }
+})
+let Facing_L = false
+let mySprite: Sprite = null
+let Facing_R = false
+Facing_R = true
 tiles.setCurrentTilemap(tilemap`level`)
-let mySprite = sprites.create(assets.image`Pirate`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`Pirate`, SpriteKind.Player)
+music.play(music.createSong(assets.song`mySong`), music.PlaybackMode.LoopingInBackground)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Pirate Stand`,
+200,
+true
+)
+controller.moveSprite(mySprite, 50, 50)
