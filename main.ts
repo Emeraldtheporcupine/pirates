088aa92ff8486e1f7cef6ad86baeebcd7c27de64
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2024-07-13 23:04:51 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,68 +1,19 @@
+namespace SpriteKind {
+    export const Cursor = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (Facing_R == true) {
-        Facing_R = true
-        Facing_L = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Walk`,
-        200,
-        true
-        )
-    } else if (Facing_L == true) {
-        Facing_L = true
-        Facing_R = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Walk0`,
-        200,
-        true
-        )
+    if (mySprite.y == 65) {
+        mySprite.y += -25
+    } else if (mySprite.y == 40) {
+    	
     }
 })
-controller.up.onEvent(ControllerButtonEvent.Released, function () {
-    if (Facing_R == true) {
-        Facing_R = true
-        Facing_L = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Stand`,
-        200,
-        true
-        )
-    } else if (Facing_L == true) {
-        Facing_L = true
-        Facing_R = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Stand L`,
-        200,
-        true
-        )
-    }
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (Facing_R == true) {
-        Facing_R = true
-        Facing_L = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Swing w Sword`,
-        100,
-        false
-        )
-        music.play(music.createSoundEffect(WaveShape.Noise, 5000, 5000, 255, 0, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
-    } else if (Facing_L == true) {
-        Facing_L = true
-        Facing_R = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Swing w Sword0`,
-        100,
-        false
-        )
-        music.play(music.createSoundEffect(WaveShape.Noise, 5000, 5000, 255, 0, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
-    }
-})
+function Play () {
+    game.gameOver(true)
+}
+function Music () {
+    game.gameOver(true)
+}
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     Facing_L = true
     Facing_R = false
@@ -73,33 +24,12 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
     true
     )
 })
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    Facing_R = true
-    Facing_L = false
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`Pirate Stand`,
-    200,
-    true
-    )
-})
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    Facing_R = true
-    Facing_L = false
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`Pirate Walk`,
-    200,
-    true
-    )
-})
-controller.B.onEvent(ControllerButtonEvent.Released, function () {
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`Pirate Stand`,
-    200,
-    true
-    )
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.y == 40) {
+        Play()
+    } else if (mySprite.y == 65) {
+        Music()
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Facing_L = true
@@ -112,60 +42,20 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (Facing_R == true) {
-        Facing_R = true
-        Facing_L = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Walk`,
-        200,
-        true
-        )
-    } else if (Facing_L == true) {
-        Facing_L = true
-        Facing_R = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Walk0`,
-        200,
-        true
-        )
+    if (mySprite.y == 40) {
+        mySprite.y += 25
+    } else if (mySprite.y == 65) {
+    	
     }
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    mySprite,
-    assets.animation`Pirate Parry`,
-    100,
-    false
-    )
-})
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    if (Facing_R == true) {
-        Facing_R = true
-        Facing_L = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Stand`,
-        200,
-        true
-        )
-    } else if (Facing_L == true) {
-        Facing_L = true
-        Facing_R = false
-        animation.runImageAnimation(
-        mySprite,
-        assets.animation`Pirate Stand L`,
-        200,
-        true
-        )
-    }
-})
-let mySprite: Sprite = null
-let Facing_L = false
 let Facing_R = false
+let Facing_L = false
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`Menu`)
 game.setDialogTextColor(10)
 game.showLongText("Welcome to the unused gallery!", DialogLayout.Bottom)
 game.showLongText("Select 'Play' to play the engine.", DialogLayout.Bottom)
 game.showLongText("Select 'Music' to hear unused music.", DialogLayout.Bottom)
 game.showLongText("Have fun! :)", DialogLayout.Bottom)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Cursor)
+mySprite.setPosition(30, 65)
