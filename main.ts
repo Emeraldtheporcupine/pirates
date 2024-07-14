9241ce6364b2b9eafbe978126ac1bd452534adc2
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2024-07-14 15:34:59 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -16,16 +16,20 @@ function Play () {
     characterAnimations.rule(Predicate.NotMoving)
     )
     pause(600)
-    textSprite = textsprite.create("")
+    game.setGameOverMessage(true, "Not Done Yet.")
+    game.gameOver(true)
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.y == 40) {
         Play()
     } else if (mySprite.y == 65) {
         Music()
+    } else if (In_Music > 0) {
+        Play_Music()
     }
 })
 function Music () {
+    In_Music = 1
     characterAnimations.runFrames(
     mySprite,
     assets.animation`myAnim`,
@@ -33,8 +37,26 @@ function Music () {
     characterAnimations.rule(Predicate.NotMoving)
     )
     pause(600)
-    game.gameOver(true)
+    scene.setBackgroundImage(assets.image`myImage0`)
+    textSprite = textsprite.create("Song " + Music_Counter, 1, 15)
+    Music_Counter_Max = 8
+    textSprite.setPosition(75, 60)
+    textSprite.setPosition(75, 60)
 }
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (In_Music > 0) {
+        sprites.destroy(textSprite)
+        Music_Counter += 1
+        textSprite = textsprite.create("Song " + Music_Counter, 1, 15)
+        textSprite.setPosition(75, 60)
+        if (Music_Counter_Max < Music_Counter) {
+            sprites.destroy(textSprite)
+            Music_Counter = 1
+            textSprite = textsprite.create("Song " + Music_Counter, 1, 15)
+            textSprite.setPosition(75, 60)
+        }
+    }
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.y == 40) {
         mySprite.y += 25
@@ -42,8 +64,16 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     	
     }
 })
+function Play_Music () {
+	
+}
+let Music_Counter_Max = 0
 let textSprite: TextSprite = null
 let mySprite: Sprite = null
+let Music_Counter = 0
+let In_Music = 0
+In_Music = 0
+Music_Counter = 1
 scene.setBackgroundImage(assets.image`Menu`)
 game.setDialogTextColor(10)
 game.showLongText("Welcome to the unused gallery!", DialogLayout.Bottom)
